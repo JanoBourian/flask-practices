5e6d97d32e30187040b165363b7bb1f9f8e52873
JanoBourian <supermath33@icloud.com>
2022-01-06 16:46:10 -0600

    Add: Until OPP

---
+++ before-all/07_unpacking_arguments.py
@@ -0,0 +1,11 @@
+def information(*args):
+    for arg in args: 
+        print(arg)
+
+information(1, 3, 6, 7, 'Abelardo')
+
+def users(**kwargs):
+    for k in kwargs.values():
+        print(k)
+
+users(name="bob", age=19)
