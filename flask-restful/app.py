fb463a8cbd1968caf836f26d1e7293bcccaa1cf4
JanoBourian <supermath33@icloud.com>
2022-01-19 18:37:39 -0600

    Add: Until GET and POST endpoints

--- flask-restful/app.py
+++ flask-restful/app.py
@@ -1,12 +1,16 @@
-from flask import Flask
+from flask import (Flask,  jsonify, request)
 from flask_restful import (
     Resource,
     Api,
 )
+import logging 
+
+INTERNAL_SERVER_ERROR = {"Error": "Internal Server Error"}
 
 app = Flask(__name__)
 api = Api(app)
 
+items = []
 
 class Student(Resource):
     def get(self, name):
@@ -16,6 +20,48 @@ class Student(Resource):
 # http://127.0.0.1:5000/student/Rolf
 api.add_resource(Student, "/student/<string:name>")
 
+
+class Item(Resource):
+    def get(self, name):
+        try:
+            for item in items: 
+                if item['name'] == name: 
+                    return item, 200
+            return {"message": "Item not found"}, 404
+        except Exception as e: 
+            logging.error(f"Error {e}")
+            return INTERNAL_SERVER_ERROR, 500
+            
+    
+    def post(self, name):
+        try:
+            data = request.get_json()
+            price = data.get('price', '')
+            if not price:
+                return {"message": "Incorrect Payload"}, 400
+            for item in items: 
+                if item['name'] == name: 
+                    return {"message": "Item already exists"}, 409
+            item = {"name": name, "price": price} 
+            items.append(item)
+            return item, 201
+        except Exception as e: 
+            logging.error(f"Error {e}")
+            return INTERNAL_SERVER_ERROR, 500
+
+# http://192.168.0.20:8080/item/<string:name>
+api.add_resource(Item, "/item/<string:name>")
+
+class ItemList(Resource):
+    def get(self):
+        try:
+            return {"items": items}, 200
+        except Exception as e: 
+            logging.error(f"Error {e}")
+            return INTERNAL_SERVER_ERROR, 500
+
+# http://192.168.0.20:8080/items
+api.add_resource(ItemList, "/items")
 
 if __name__ == "__main__":
     host = '192.168.0.20'
