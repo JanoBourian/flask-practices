d60962c9dae21b7f05a381b8a556c1cfe328b285
JanoBourian <supermath33@icloud.com>
2022-02-17 18:35:24 -0600

    Feature: Stores completed

--- flask-app/models/ItemModel.py
+++ flask-app/models/ItemModel.py
@@ -2,6 +2,15 @@ from db import db
 
 
 class ItemModel(db.Model):
+    
+    __tablename__ ="items"
+    id = db.Column(db.Integer, primary_key=True)
+    sku = db.Column(db.String(10), unique=True, nullable=False)
+    name = db.Column(db.String(30))
+    price = db.Column(db.Float(precision=2))
+    store_id = db.Column(db.Integer, db.ForeignKey("stores.id"))
+    store = db.relationship('StoreModel')
+    
     def __init__(self, sku, name, price, store_id):
         self.sku = sku
         self.name = name
--- flask-app/resources/StoreList.py
+++ flask-app/resources/StoreList.py
@@ -1,9 +1,17 @@
-from flask_restful import Resource, reqparse
-
+from flask_restful import Resource
+from models.StoreModel import StoreModel
 
 class StoreList(Resource):
     def get(self):
         try:
-            return {"stores": []}, 200
+            stores = StoreModel.get_all_elements()
+            res = []
+            for store in stores:
+                data = {
+                    "name": store.name, 
+                    "description": store.description
+                }
+                res.append(data)
+            return {"stores": res}, 200
         except Exception as e:
             return {"error": e}, 500
