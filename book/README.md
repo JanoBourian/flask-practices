1611271e8073fda7be395ad1d1a1e176cb02d252
JanoBourian <supermath33@icloud.com>
2022-03-23 11:26:02 -0600

    Add: Bootstrap, until utc time!

--- book/README.md
+++ book/README.md
@@ -62,3 +62,4 @@
 
 ## Installations
     - flask
+    - flask-bootstrap
--- book/app.py
+++ book/app.py
@@ -2,13 +2,27 @@ from flask import Flask, request, make_response, render_template
 
 app = Flask(__name__)
 
+user_list = [
+    {'name': "Jonh", "filePhotoName": "jonh_book"},
+    {'name': "Carl", "filePhotoName": "carl_book"},
+    {'name': "Gretta", "filePhotoName": "gretta_book"},
+    ]
+
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('index.html', user_list=user_list)
 
 @app.route("/user/<name>")
 def user(name):
     return render_template('user.html', name=name)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_error_server(e):
+    return render_template('500.html'), 500
 
 if __name__ =="__main__":
     args = {
