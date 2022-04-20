89337aa3981d9af1261cf0c0d38e639a57c28907
JanoBourian <supermath33@icloud.com>
2022-04-20 16:52:18 -0500

    Add: first files for large-app

--- large-app/flasky.py
+++ large-app/flasky.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -6,5 +6,15 @@ app = Flask(__name__)
 def home():
     return "<h1> Hello world </h1>"
 
+@app.route("/user/<string:name>")
+def user(name:str):
+    # print(request.args)
+    return f"<h1> Hello {name}</h1>"
+
 if __name__ == '__main__':
-    app.run()
+    args = {
+    "debug":True,
+    "port": '8000',
+    "host": '192.168.0.26'
+    }
+    app.run(**args)
