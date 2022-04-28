6fb99f61253b53969c2e6a731c63882261041acb
JanoBourian <supermath33@icloud.com>
2022-04-27 19:58:44 -0500

    Add: I removed the unecessary lines in the config file

--- large-app/config.py
+++ large-app/config.py
@@ -5,7 +5,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir,'.env'))
 
 class Config:
-    # TEMPLATES_FOLDER = os.environ.get('TEMPLATES_FOLDER')
     
     @staticmethod
     def init_app(app):
@@ -14,7 +13,6 @@ class Config:
 class DevelopmentConfig(Config):
     DEBUG = os.environ.get('DEBUG', False)
     FLASK_ENV = os.environ.get('FLASK_ENV_DEV')
-    # SERVER_NAME = os.environ.get('SERVER_NAME')
     
 
 config = {
