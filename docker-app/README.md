6383cc16fae899bf3eec17444eaed4d5b08afb8e
JanoBourian <supermath33@icloud.com>
2022-05-03 20:53:27 -0500

    Add: I added someone useful commands to request

--- docker-app/README.md
+++ docker-app/README.md
@@ -15,6 +15,30 @@
     - set FLASK_APP=app.py (or export)
     - set FLASK_DEBUG=1
     - flask run
+    - dynamic routes
+
+### Useful commands
+    - from flask
+        - Flask (The main instance)
+        - make_response()
+        - abort()
+    - from context
+        - current_app
+        - g
+        - request
+        - session
+    - from request
+        - form
+        - args
+        - cookies
+        - headers
+        - file
+        - get_data()
+        - get_json()
+        - blueprint
+        - endpoint
+        - method
+    - from response
 
 ### Requirements.txt
     - flask
--- docker-app/flasky.py
+++ docker-app/flasky.py
@@ -1,10 +1,35 @@
-from flask import Flask
+from flask import Flask, make_response, abort, request
 
 app = Flask(__name__)
 
 @app.route("/", methods=['GET'])
 def index():
     return "<h1> Hello World! </h1>"
+
+@app.route("/user/<string:name>", methods=['GET'])
+def user(name):
+    is_json = make_response({"message": f"{name}"})
+    print(type(is_json))
+    print(type(is_json.get_json()))
+    print(request)
+    print(request.form)
+    print(request.args)
+    print(request.cookies)
+    print(request.headers)
+    print(type(request.headers))
+    print(request.headers.get('Host', ''))
+    print(request.files)
+    print(request.get_data())
+    print(request.blueprint)
+    print(request.endpoint)
+    print(request.method)
+    print(request.scheme)
+    return is_json
+
+@app.route("/abort", methods=['GET'])
+def abort_rule():
+    # abort(400, description="This is not necessary")    
+    abort(500, "My custom message")
 
 if __name__ == "__main__":
     app.run()
