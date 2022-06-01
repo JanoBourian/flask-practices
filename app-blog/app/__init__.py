7b4891fb002c69c5593f84e0b765de8984fef3ae
JanoBourian <supermath33@icloud.com>
2022-06-01 13:15:56 -0500

    Add: I added the email sender

--- app-blog/app/__init__.py
+++ app-blog/app/__init__.py
@@ -1,20 +1,25 @@
 from flask import Flask
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail
 from config import config
 
 moment = Moment()
 db = SQLAlchemy()
+mail = Mail()
 
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
+    app.debug=1
     
     # init_app(app) configuration
     moment.init_app(app)
     db.init_app(app)
+    mail.init_app(app)
     
+    print(f"Config: {app.config}")
     # attach routes and custom error pages here
     
     from .main import main as main_blueprint
--- app-blog/app/main/views.py
+++ app-blog/app/main/views.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from flask import render_template, session, flash, redirect, url_for, current_app
 from .forms import NameForm
+from ..email import send_email
 from . import main 
 from .. import db
 from ..models import User, Role
@@ -15,6 +16,9 @@ def index():
             db.session.add(user)
             db.session.commit()
             session['known'] = False
+            if current_app.config['FLASKY_ADMIN']:
+                send_email(current_app.config['FLASKY_ADMIN'], 'New User',
+                           'mail/new_user', user=user)
             flash('Looks like you have changed your name!')
         else:
             session['known'] = True
