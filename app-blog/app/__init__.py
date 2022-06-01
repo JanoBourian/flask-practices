b965984dc2a7545341ede7c3f473db93fab52c83
JanoBourian <supermath33@icloud.com>
2022-06-01 13:03:02 -0500

    Add: I added form and database

--- app-blog/app/__init__.py
+++ app-blog/app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask_moment import Moment
+from flask_sqlalchemy import SQLAlchemy
 from config import config
 
 moment = Moment()
+db = SQLAlchemy()
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -11,6 +13,7 @@ def create_app(config_name):
     
     # init_app(app) configuration
     moment.init_app(app)
+    db.init_app(app)
     
     # attach routes and custom error pages here
     
--- app-blog/app/main/views.py
+++ app-blog/app/main/views.py
@@ -1,8 +1,24 @@
 from datetime import datetime
-from flask import render_template
+from flask import render_template, session, flash, redirect, url_for, current_app
 from .forms import NameForm
 from . import main 
+from .. import db
+from ..models import User, Role
 
 @main.route("/", methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time=datetime.utcnow()), 200
+    form = NameForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            db.session.commit()
+            session['known'] = False
+            flash('Looks like you have changed your name!')
+        else:
+            session['known'] = True
+        session['name'] = form.name.data
+        form.name.data = ''
+        return redirect(url_for("main.index"))
+    return render_template('index.html', current_time=datetime.utcnow(), known=session.get('known', False), form = form, name=session.get('name', '')), 200
